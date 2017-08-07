99d01f64711482afd54bc1fc02ffc7cdad7ec4b1
Tomasz Skorupa <2433416+roverslim@users.noreply.github.com>
2017-08-07 19:22:31 -0400

    drop tmux from install script

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,6 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -sfv "$DOTFILES_DIR/.vim" ~
 ln -sfv "$DOTFILES_DIR/.vimrc" ~
 
-ln -sfv "$DOTFILES_DIR/.tmux.conf" ~
-
 ln -sfv "$DOTFILES_DIR/.ackrc" ~
 
 ln -sfv "$DOTFILES_DIR/.vagrant.d/Vagrantfile" ~/.vagrant.d
